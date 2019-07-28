1b994ed875ecc087da5df26587d335fb827aef3f
Alex Rhodes <anrhodes5738@gmail.com>
2019-07-28 10:48:23 -0500

    Initial Commit

---
+++ AnywhereFitness/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnywhereFitness
+//
+//  Created by Alex Rhodes on 7/28/19.
+//  Copyright © 2019 Alex Rhodes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
