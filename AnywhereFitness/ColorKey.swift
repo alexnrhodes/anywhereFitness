3c736e92440bee049ed2bb7cfe50ac69a506cc08
Alex Rhodes <anrhodes5738@gmail.com>
2019-07-29 15:09:10 -0500

    Most of the storyboard complete

---
+++ AnywhereFitness/ColorKey.swift
@@ -0,0 +1,16 @@
+//
+//  ColorKey.swift
+//  AnywhereFitness
+//
+//  Created by Alex Rhodes on 7/28/19.
+//  Copyright © 2019 Alex Rhodes. All rights reserved.
+//
+
+import UIKit
+
+struct ColorKey {
+   static let tan = #colorLiteral(red: 0.8418782949, green: 0.8107753396, blue: 0.6764074564, alpha: 1)
+   static let seafoamGreen = #colorLiteral(red: 0, green: 0.9234121442, blue: 0.7218109965, alpha: 1)
+    
+    
+}
--- AnywhereFitness/ViewController.swift
+++ AnywhereFitness/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
 }
+
 
